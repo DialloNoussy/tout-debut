f1b43d9c4c4f8c934b330efc7feafecf2311539b
DialloNoussy <abdourahmane.noussydiallo@gmail.com>
2022-07-14 20:56:30 +0200

    just
    just

---
+++ script.js
@@ -0,0 +1,25 @@
+const espaceMessage = document.getElementById("message");
+const bouton = document.getElementById("bouton");
+const ageInput = document.getElementById("age");
+let age;
+let ageMajorite = 18;
+function valider(){
+  espaceMessage.innerHTML = "Vous êtes autorisé à entrer🎖️🎖️🎖️";
+}
+function refuser(){
+  alert("Cette espace est interdit aux personnes mineurs 🚨🚔👮‍♀️😁");
+}
+function onConfirm(){
+  age = parseInt(ageInput.value);
+  if(isNaN(age)){
+    alert("Ceci n'est pas un nombre");
+    return;
+  }
+  if(age < ageMajorite){
+    refuser();
+  }else{
+    valider()
+  }
+  ageInput.value = "";
+}
+bouton.addEventListener('click', onConfirm);
